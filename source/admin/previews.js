3f6435b3c7b2fc308aac65d19bb2a2594ee86ff8
Marcelo Moreira <marcelo.moreira@dnsw.com.au>
2019-10-21 17:56:06 +1100

    revert preview

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -64,28 +64,6 @@ const HomePreview = ({ entry, widgetFor }) => {
   )
 }
 
-const AboutPreview = ({ entry, widgetFor }) => {
-    const data = entry.get('data').toJS()
-    return (
-        <div>
-        <section className="hero" style={{ backgroundImage: `url('${data.hero.image}')` }}>
-<div className="caption">
-        <div className="row">
-        <h1>{data.hero.title}</h1>
-        </div>
-        </div>
-        </section>
-        <section className="homepage-story">
-        <div className="container">
-        <div className="row">
-        <div className="story-content">{widgetFor('story')}</div>
-    </div>
-    </div>
-    </section>
-    </div>
-)
-}
-
 const NavigationPreview = ({ entry }) => {
   const data = entry.get('data').toJS()
   return (
