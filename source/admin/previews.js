23d2355d051b2d5ca712e21e934626dfbd45c2cf
Marcelo Moreira <marcelo.moreira@dnsw.com.au>
2019-10-21 17:53:51 +1100

    test preview

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -64,13 +64,35 @@ const HomePreview = ({ entry, widgetFor }) => {
   )
 }
 
+const AboutPreview = ({ entry, widgetFor }) => {
+    const data = entry.get('data').toJS()
+    return (
+        <div>
+        <section className="hero" style={{ backgroundImage: `url('${data.hero.image}')` }}>
+<div className="caption">
+        <div className="row">
+        <h1>{data.hero.title}</h1>
+        </div>
+        </div>
+        </section>
+        <section className="homepage-story">
+        <div className="container">
+        <div className="row">
+        <div className="story-content">{widgetFor('story')}</div>
+    </div>
+    </div>
+    </section>
+    </div>
+)
+}
+
 const NavigationPreview = ({ entry }) => {
   const data = entry.get('data').toJS()
   return (
     <header className="header-primary">
       <div className="logo">
         <a href="/">
-          <img src="/images/logo.svg" alt="Kaldi"/>
+          <img src="/images/logo-rectangular.png" alt="SukaBio"/>
         </a>
       </div>
       <a className="nav-button">
